ba091a79809cfb2c23c50f72e94a475447b1bd42
“Semih <semih.uygun@student.pxl.be>
2023-06-06 00:03:01 +0200

    SquareDigits challenge

---
+++ CodingChallenges/ChallengeClasses/SquareDigits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges.ChallengeClasses
+{
+    // challenge link https://www.codewars.com/kata/546e2562b03326a88e000020/train/csharp
+    public class SquareDigits
+    {
+        // my solution
+        public static int SquareDigit(int n)
+        {
+            string total = string.Empty;
+            string number = n.ToString();
+            foreach(char c in number)
+            {
+                total += (Char.GetNumericValue(c) * Char.GetNumericValue(c)).ToString();
+            }
+            return Convert.ToInt32(total);
+        }
+
+        // solution voted as best practice
+        public static int SquareDigitsBestPractice(int n)
+        {
+            string output = "";
+            foreach (char c in n.ToString())
+            {
+                int square = int.Parse(c.ToString());
+                output += (square * square);
+            }
+            return int.Parse(output);
+        }
+    }
+}
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingChallenges.ChallengeClasses;
+
 Console.WriteLine("Hello, World!");
+var test =SquareDigits.SquareDigit(9119);
+Console.WriteLine(test);
