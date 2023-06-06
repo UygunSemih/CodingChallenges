1024146eb5386c467cf6544d79252a3aaad38e9e
“Semih <semih.uygun@student.pxl.be>
2023-06-06 17:58:13 +0200

    MexicanWave challenge completed

---
+++ CodingChallenges/ChallengeClasses/6KYU/MexicanWave.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges.ChallengeClasses._6KYU
+{
+    // challenge link https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/csharp
+    public class MexicanWave
+    {
+        // my solution
+        public List<string> wave(string str)
+        {
+            var list = new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Equals(' '))
+                    continue;
+                char[] ch = str.ToCharArray();
+                ch[i] = Convert.ToChar(ch[i].ToString().ToUpper());
+                list.Add(new string(ch));
+            }
+            return list;
+        }
+
+        // solution voted as best practice
+        public List<string> waveBestPractice(string str) =>
+          str
+            .Select((c, i) => str.Substring(0, i) + Char.ToUpper(c) + str.Substring(i + 1))
+            .Where(x => x != str)
+            .ToList();
+    }
+}
