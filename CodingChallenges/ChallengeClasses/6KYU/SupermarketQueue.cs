7bde53a91a759c9a0841e5dac4aca918c320a3f1
“Semih <semih.uygun@student.pxl.be>
2023-06-06 18:17:41 +0200

    SupermarketQueue challenge completed

---
+++ CodingChallenges/ChallengeClasses/6KYU/SupermarketQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges.ChallengeClasses._6KYU
+{
+    // challenge link https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/csharp
+    public class SupermarketQueue
+    {
+        // my solution
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] tills = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                tills[i] = 0;
+            }
+            foreach (int customer in customers)
+            {
+                int quickest = Array.IndexOf(tills, tills.Min());
+                tills[quickest] += customer;
+            }
+            return tills.Max();
+        }
+
+        // solution voted as best practice
+        public static long QueueTimeBestPractice(int[] customers, int n)
+        {
+            var registers = new List<int>(Enumerable.Repeat(0, n));
+
+            foreach (int cust in customers)
+            {
+                registers[registers.IndexOf(registers.Min())] += cust;
+            }
+            return registers.Max();
+        }
+    }
+}
