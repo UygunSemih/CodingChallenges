5dd43ef3be4f231cc87b451682d1afc6385afd53
“Semih <semih.uygun@student.pxl.be>
2023-06-12 19:11:18 +0200

    completed sum strings as numbers challenge.

---
+++ CodingChallenges/ChallengeClasses/4KYU/SumStringsAsNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace CodingChallenges.ChallengeClasses._4KYU
+{
+    // challenge link https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/csharp
+    public class SumStringsAsNumbers
+    {
+        // my solution
+        public static string sumStrings(string a, string b)
+        {
+
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+
+
+        // solution voted as best practice
+        public static int GetLastDigitBestPractice(BigInteger n1, BigInteger n2)
+        {
+            return (int)BigInteger.ModPow(n1, n2, 10);
+        }
+    }
+}
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -2,7 +2,9 @@
 using CodingChallenges.ChallengeClasses._5KYU;
 using CodingChallenges.ChallengeClasses._6KYU;
 using CodingChallenges.ChallengeClasses._7KYU;
+using System.Numerics;
 
+BigInteger.Parse("-110");
 var test = RangeExtraction.Extract(new[] { -100, -99,-97,-96,-94 });
 Console.WriteLine(test);
 Console.ReadLine();
