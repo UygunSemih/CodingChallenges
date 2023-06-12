c4b395a002b7704bb0a9b70f1faea80f0f7f4a23
“Semih <semih.uygun@student.pxl.be>
2023-06-12 20:44:38 +0200

    PigLatin challenge solved

--- CodingChallenges/ChallengeClasses/5KYU/MovingZerosToTheEnd.cs
+++ CodingChallenges/ChallengeClasses/5KYU/MovingZerosToTheEnd.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodingChallenges.ChallengeClasses._5KYU
 {
-    internal class MovingZerosToTheEnd
+    // challenge link https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/csharp
+    public class MovingZerosToTheEnd
     {
+        // my solution
+        public static int[] MoveZeroes(int[] arr)
+        {
+            var list = new List<int>(arr);
+            list.RemoveAll(x => x == 0);
+            while (list.Count < arr.Length)
+            {
+                list.Add(0);
+            }
+            return list.ToArray();
+        }
+
+
+        // solution voted as best practice
+        public static int[] MoveZeroesBestPractice(int[] arr)
+        {
+            return arr.OrderBy(x => x == 0).ToArray();
+        }
     }
 }
---
+++ CodingChallenges/ChallengeClasses/5KYU/PigLatin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges.ChallengeClasses._5KYU
+{
+    // challenge link https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/csharp
+    public class PigLatin
+    {
+        // my solution
+        public static string PigIt(string str)
+        {
+            string result = string.Empty;
+            var split = str.Split(' ');
+            foreach (var word in split)
+            {
+                if (!char.IsPunctuation(word[0])) 
+                    result += word.Substring(1) + word[0] + "ay";
+                else
+                    result += word;
+                result += ' ';
+            }
+            return result.TrimEnd();
+        }
+
+
+        // solution voted as best practice
+        public static string PigItBestPractice(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(w => w.Any(char.IsPunctuation) ? w : w.Substring(1) + w[0] + "ay"));
+        }
+    }
+}
